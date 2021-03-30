5b9861a20ef37f0e175f2eb4a58cd88f4f606361
Rodhenrique <henriquesilvasenai@gmail.com>
2021-03-29 23:53:49 -0300

    Finish

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,5 +1,8 @@
 "use strict";
 
+var item_projetos = document.getElementsByClassName("portfolio-details");
+var num = 0;
+
 window.onload = function () {
   var icon = document.getElementById("menu");
   var nav = document.getElementById("navbar");
@@ -22,4 +25,97 @@ window.onload = function () {
       icon.classList.add("fa-bars");
     });
   });
+  var close = document.getElementById("close_box");
+  close.addEventListener("click", function () {
+    item_projetos[parseInt(num)].style.display = "none";
+    close.style.display = "none";
+    document.getElementById("header").style.display = "flex";
+    document.getElementsByTagName("main")[0].style.display = "block";
+  });
 };
+
+var slideIndex = 0;
+var slides, dots;
+
+function openBox(item, slide, point) {
+  var close = document.getElementById("close_box");
+  num = item;
+  item_projetos[parseInt(item)].style.display = "block";
+  close.style.display = "block";
+  document.getElementById("header").style.display = "none";
+  document.getElementsByTagName("main")[0].style.display = "none";
+  slides = document.getElementsByClassName(slide);
+  dots = document.getElementsByClassName(point);
+  slideIndex = 0;
+  showSlides();
+}
+
+function currentDiv(n) {
+  dotDivs(slideIndex = n);
+}
+
+function showSlides() {
+  var i;
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" points-white", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " points-white";
+  setTimeout(showSlides, 8000);
+}
+
+function dotDivs(n) {
+  var i;
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" points-white", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " points-white";
+}
+
+function plusSlides(position) {
+  slideIndex += position;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  } else if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" points-white", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " points-white";
+}
